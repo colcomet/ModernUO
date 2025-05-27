7d848af4bf1264ef32283f8862e1425623971af5
Andrew Barber <drewbarber72@gmail.com>
2025-05-26 21:29:31 -0400

    Confirm release gump now works properly when at a distance that would normally be too far to give the release command. Also added the pet name to the release gump, to make it more clear what is being release.

--- Projects/UOContent/Commands/Pets.cs
+++ Projects/UOContent/Commands/Pets.cs
@@ -30,42 +30,6 @@ public static class Pets
             e.Mobile.PrivateOverheadMessage(MessageType.Regular, MessageHues.BlueNoticeHue, false, "You have no followers.", e.Mobile.NetState);
         }
     }
-
-    public static string GetDistanceAndCompassDirectionToPet(PlayerMobile pm, BaseCreature pet)
-    {
-        if (pet == null) return string.Empty;
-        if (pet == pm.Mount)
-        {
-            return "(mounted)";
-        }
-        else if (pm.Map == pet.Map)
-        {
-            var distance = (int)(pm.GetDistanceToSqrt(pet));
-            var direction = pm.GetDirectionTo(pet);
-            var dirString = direction.ToString();
-            if (direction == Direction.Up)
-            {
-                dirString = "Northwest";
-            }
-            else if (direction == Direction.Right)
-            {
-                dirString = "Northeast";
-            }
-            else if (direction == Direction.Left)
-            {
-                dirString = "Southwest";
-            }
-            else if (direction == Direction.Down)
-            {
-                dirString = "Southeast";
-            }
-            return $"{distance} tiles, {dirString} ({pet.Region.Name})";
-        }
-        else
-        {
-            return $"in {pet.Map.Name}";
-        }
-    }
 }
 
 public class PetListGump : Gump
@@ -73,46 +37,6 @@ public class PetListGump : Gump
     private readonly Mobile m_From;
     private readonly List<Mobile> m_List;
 
-    public static string GetPetEntryText(Mobile pm, BaseCreature pet)
-    {
-        string baseString = $"{pet.Name} (s: {pet.ControlSlots}) ";
-        if (pet == null) return string.Empty;
-        if (pet == pm.Mount)
-        {
-            return baseString + "(mounted)";
-        }
-        else if (pm.Map == pet.Map)
-        {
-            var distance = (int)(pm.GetDistanceToSqrt(pet));
-            var direction = pm.GetDirectionTo(pet);
-            var dirString = direction.ToString();
-            if (distance == 0)
-            {
-                return baseString + "(standing on your feet)";
-            }
-            else if (direction == Direction.Up)
-            {
-                dirString = "Northwest";
-            }
-            else if (direction == Direction.Right)
-            {
-                dirString = "Northeast";
-            }
-            else if (direction == Direction.Left)
-            {
-                dirString = "Southwest";
-            }
-            else if (direction == Direction.Down)
-            {
-                dirString = "Southeast";
-            }
-            return baseString + $"({pet.X}, {pet.Y}, {pet.Z}), {distance} tiles, {dirString} ({pet.Region.Name})";
-        }
-        else
-        {
-            return baseString + $"({pet.X}, {pet.Y}, {pet.Z}) in {pet.Map.Name}";
-        }
-    }
     public override bool Singleton => true;
 
     public PetListGump(Mobile from, HashSet<Mobile> mlist) : base(50, 50)
@@ -150,13 +74,52 @@ public class PetListGump : Gump
         {
             return;
         }
-        if(pm.Mount == m_List[index])
+        var pet = m_List[index] as BaseCreature;
+        if (pm.Mount == pet)
         {
-            pm.PrivateOverheadMessage(MessageType.Regular, MessageHues.RedErrorHue, false, $"You can't release {m_List[index].Name} while riding them!", pm.NetState);
+            pm.PrivateOverheadMessage(MessageType.Regular, MessageHues.RedErrorHue, false, $"You can't release {pet.Name} while riding them!", pm.NetState);
             return;
         }
-        pm.PrivateOverheadMessage(MessageType.Regular, MessageHues.BlueNoticeHue, false, $"{m_List[index].Name} is now free!", pm.NetState);
-        pm.SendGump(new ConfirmReleaseGump(pm, (BaseCreature)m_List[index]));
-        
+        pm.SendGump(new ConfirmReleaseGump(pm, pet, checkRange: false));
+    }
+    public static string GetPetEntryText(Mobile pm, BaseCreature pet)
+    {
+        if (pet == null) return string.Empty;
+        string baseString = $"{pet.Name} (s: {pet.ControlSlots}) ";
+        if (pet == pm.Mount)
+        {
+            return baseString + "(mounted)";
+        }
+        else if (pm.Map == pet.Map)
+        {
+            var distance = (int)(pm.GetDistanceToSqrt(pet));
+            var direction = pm.GetDirectionTo(pet);
+            var dirString = direction.ToString();
+            if (distance == 0)
+            {
+                return baseString + "(standing on your feet)";
+            }
+            else if (direction == Direction.Up)
+            {
+                dirString = "Northwest";
+            }
+            else if (direction == Direction.Right)
+            {
+                dirString = "Northeast";
+            }
+            else if (direction == Direction.Left)
+            {
+                dirString = "Southwest";
+            }
+            else if (direction == Direction.Down)
+            {
+                dirString = "Southeast";
+            }
+            return baseString + $"({pet.X}, {pet.Y}, {pet.Z}), {distance} tiles, {dirString} ({pet.Region.Name})";
+        }
+        else
+        {
+            return baseString + $"({pet.X}, {pet.Y}, {pet.Z}) in {pet.Map.Name}";
+        }
     }
 }
--- Projects/UOContent/Gumps/ConfirmReleaseGump.cs
+++ Projects/UOContent/Gumps/ConfirmReleaseGump.cs
@@ -1,3 +1,5 @@
+using System;
+using MimeKit.Text;
 using Server.Mobiles;
 using Server.Network;
 
@@ -7,41 +9,55 @@ public class ConfirmReleaseGump : StaticGump<ConfirmReleaseGump>
 {
     private readonly Mobile _from;
     private readonly BaseCreature _pet;
+    private readonly bool _checkRange = true;
 
     public override bool Singleton => true;
 
-    public ConfirmReleaseGump(Mobile from, BaseCreature pet) : base(50, 50)
+    public ConfirmReleaseGump(Mobile from, BaseCreature pet, bool checkRange = true) : base(50, 50)
     {
         _from = from;
         _pet = pet;
+        _checkRange = checkRange;
+
     }
 
     protected override void BuildLayout(ref StaticGumpBuilder builder)
     {
         builder.AddPage();
 
-        builder.AddBackground(0, 0, 270, 120, 5054);
-        builder.AddBackground(10, 10, 250, 100, 3000);
+        builder.AddBackground(0, 0, 270, 170, 5054);
+        builder.AddBackground(10, 10, 250, 150, 3000);
 
         // Are you sure you want to release your pet?
-        builder.AddHtmlLocalized(20, 15, 230, 60, 1046257, true, true);
+        //builder.AddHtmlLocalized(20, 15, 230, 110, 1046257, true, true);
+        ReadOnlySpan<char> htmlText = $"Are you sure you wish to release {_pet.Name}? If they are summoned, they will immediately disappear.";
+        builder.AddHtml(20, 15, 230, 110, htmlText, true, true);
 
-        builder.AddButton(20, 80, 4005, 4007, 2);
-        builder.AddHtmlLocalized(55, 80, 75, 20, 1011011); // CONTINUE
+        builder.AddButton(20, 130, 4005, 4007, 2);
+        builder.AddHtmlLocalized(55, 130, 75, 20, 1011011); // CONTINUE
 
-        builder.AddButton(135, 80, 4005, 4007, 1);
-        builder.AddHtmlLocalized(170, 80, 75, 20, 1011012); // CANCEL
+        builder.AddButton(135, 130, 4005, 4007, 1);
+        builder.AddHtmlLocalized(170, 130, 75, 20, 1011012); // CANCEL
     }
 
     public override void OnResponse(NetState sender, in RelayInfo info)
     {
-        if (info.ButtonID != 2 || _pet.Deleted ||
-            !(_pet.Controlled && _from == _pet.ControlMaster &&
-              _from.CheckAlive() && _pet.Map == _from.Map && _pet.InRange(_from, 14)))
+        if (
+                info.ButtonID != 2 || _pet.Deleted ||
+                !(
+                    _pet.Controlled && _from == _pet.ControlMaster &&
+                    _from.CheckAlive() &&
+                    (
+                        !_checkRange ||
+                        (_pet.Map == _from.Map && _pet.InRange(_from, 14))
+                    )
+                 )
+           )
         {
+            _from.SendMessage("You decide not to release your pet.");
             return;
         }
-
+        _from.PrivateOverheadMessage(MessageType.Regular, MessageHues.BlueNoticeHue, false, $"You release {_pet.Name}!", _from.NetState);
         _pet.ControlTarget = null;
         _pet.ControlOrder = OrderType.Release;
     }
